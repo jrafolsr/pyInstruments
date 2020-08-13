dd6ff6eebab53206e257073761fa9ee7690bb6ad
Joan <joanrafols1988@gmail.com>
2020-08-13 16:32:31 +0200

    Some changes to account for the reorganization.

--- __init__.py
+++ __init__.py
@@ -6,9 +6,8 @@ Created on Fri Nov 29 16:38:13 2019
 """
 
 from .resources import sourcemeter, list_resources
-from .pid import pid
+from . import pid
 from . import instruments
-from . import pid_controls
-from . import global_settings_pid, global_settings_ivl, global_settings_iv
+from . import  global_settings_ivl, global_settings_iv, global_settings_pid
 from . import ivl_setups
 from . import configurations
