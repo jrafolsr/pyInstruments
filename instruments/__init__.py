eff26bba0d96ade3fab3edd9bb2c0de40e5ff9fd
Joan <joanrafols1988@gmail.com>
2020-11-11 11:08:21 +0100

    Comment DAQ module.

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -7,7 +7,7 @@ Created on Fri Nov 29 17:05:08 2019
 
 from .keithley24XX import keithley24XX
 from .keysight34461A import keysight34461A
-from .ni_usb_daq import daq
+#from .ni_usb_daq import daq
 from .keysight33500B import keysight33500B
 from .nanovoltmeter import nanovoltmeter
 from .agilentE3631A import agilentE3631A
