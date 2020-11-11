8b2a3246acf9bbf8a4a7a93825410d1f03c73257
Joan <joanrafols1988@gmail.com>
2020-11-11 11:07:37 +0100

    Update the activation of the conda environment through conda.bat instead of just conda.

--- ivlogger/KeithleyLogger.bat
+++ ivlogger/KeithleyLogger.bat
@@ -1,8 +1,8 @@
 @echo off
 ECHO Activating the conda environment...
-call conda activate
+call conda.bat activate
 ECHO Launching the program...
 ECHO Launching the browser...
 "C:\Program Files\Mozilla Firefox\firefox" http://127.0.0.1:8053/ -ftimeout=10 
 python IV_GUI.py
-call conda deactivate
+call conda.bat deactivate
