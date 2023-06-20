e50a185339ba0dbb3480505b8b458cc86a8e53ff
Joan <joanrafols1988@gmail.com>
2023-06-20 19:20:25 +0200

    Added methods for sync measurements

--- instruments/keithley24XX.py
+++ instruments/keithley24XX.py
@@ -131,7 +131,7 @@ class keithley24XX(sourcemeter):
             HAVE FUN! """
         if reset:
             self.inst.write("*RST")                  # Reset instrument to default parameters if reset flag is True.
-            self.inst.write(":SYSTem:TIME:RESet")   # Reset the time of the sourcemeter
+#            self.inst.write(":SYSTem:TIME:RESet")   # Reset the time of the sourcemeter
         self.inst.write(":SYST:BEEP:STAT %i" % beeper) # Turn on/off the beeper
         self.inst.write(":ROUT:TERM %s" % term)     # Set the route to term front/rear
         self.inst.write(":SYST:RSEN %i" % fw)         # Select four wire measuremnt ohms mode. 
@@ -307,4 +307,186 @@ class keithley24XX(sourcemeter):
         
         return data
         
+    def configure_syncsweep_master(self,start, stop, step = 0.1, mode = 'step', sweep_list = [], term = 'FRONT', cmpl = 0.1, delay = 0.1, ranging = 'AUTO', nplc = 1, spacing = 'LIN', reset = True, stay_on = False, source_range = 'BEST'):
+        """
+        Configures the Keithley to perform a voltage sweep
+    
+        Parameters:
+        ----------
+        start: int ot float
+            Initial voltage in V in case of a stair-like sweep.
+        stop: int ot float
+            Final voltage in V in case of a stair-like sweep.           
+        step: int ot float
+            Voltage step in V in case of a stair-like sweep.
+        mode: 'step' or 'list'
+            Sweep mode. If 'step' the values start, stop, step and spacing are used to configure the sweep-list values. If 'list' the list passed to the 'sweep_list' argument will be used. The default is 'step'.
+        term: 'FRONT' ot 'REAR'
+            The output terminal to use. the defualt is 'FRONT'
+        cmpl: itn or float
+            The compliance value in A.
+        nplc: int or float 0.01 <= nplc <=100
+            Numebr of pulse light cycles to average the data. The default is 1.
+       ranging: 'AUTO' or float
+           Set the current sensign range to a fix one in the case a value is passed. The default is None and the sourcemeter will adjust the range according to the measured values.
+       spacing:'LIN'  or 'LOG'
+          Spacinf type of the values in the case of a stair-like sweep.
+       delay: float or string
+           Specify delay in seconds between the settled source value and the measurement reading if 'auto' the sourcemeter determines the best delay. The default is 0.1 s.
+       reset: bool, optional
+           Resets the instrument, erasing all precious configurations. The default is True.
+       stay_on: bool, optional
+           If True the output stays on after the sweep, if False it automatically switch off. The default is False.
+       source_ranging: 'AUTO', BEST', or float
+           Set the voltage source range to a fix one in the case a value is passed. The default is 'BEST' and the sourcemeter will adjust the range according to the measured values. 'AUTO' is also accepted.
+       
+        """ 
+        if reset:
+            self.inst.write("*RST")                  # Reset instrument to default parameters if reset flag is True.
+#            self.inst.write(":SYSTem:TIME:RESet")   # Reset the time of the sourcemeter
+            
+        self.inst.write("TRIG:CLE") # Clear any pending triggers
+        self.inst.write("SYST:azer ON") # Ensure auto zero is enabled    
+             
+        self.inst.write(":SYST:BEEP:STAT 1") # Turn on/off the beeper
+        self.inst.write(":ROUT:TERM %s" % term)     # Set the route to term front/rear 
+        self.inst.write(":SYST:RSEN 0")         # Disable four wire measuremnts
+        
+#        self.inst.write("sour:cle:auto:mode tco") # Output off after trigger count
+        
+        self.inst.write("arm:coun 1") # Perform 1 sweep per ":INIT"
+        self.inst.write("arm:sour imm") # Immediately go to Trig layer
+        self.inst.write("arm:dir ACC") # Wait for trigger event (IMM)
+        self.inst.write("arm:outp none") # No output triggers from scan
+#        self.inst.write("trig:coun 10") # Perform # points in sweep
+        self.inst.write("trig:sour tlin") # Trigger using Trigger Link
+        self.inst.write("trig:dir acc") # Skip first trigger
+        self.inst.write("trig:outp sour") # Output trigger after source "on"
+        self.inst.write("trig:inp sens") # Wait for trigger before measure
+        self.inst.write("trig:ilin 1") # Input trigger line
+        self.inst.write("trig:olin 2") # Output trigger line
+        self.inst.write("trig:del 0.0") # Set trigger delay before source
+        
+        
+        if stay_on:
+            self.inst.write(":SOUR:CLE:AUTO OFF")     # Enable source auto output-off.
+        else: 
+            self.inst.write(":SOUR:CLE:AUTO ON")     # Enable source auto output-off.
+        
+        self.inst.write(":SOUR:FUNC VOLT")
+        
+        Npoints = int((stop - start) / step) + 1
+        if mode == 'step':
+            self.inst.write(":SOURce:VOLTage:MODE SWEep")
+            self.inst.write(":SOURce:SwEep:SPACing %s" % spacing)
+            self.inst.write(":SOURce:VOLTage:STARt %.6f" % start)
+            self.inst.write(":SOURce:VOLTage:STOP %.6f" % stop)
+            self.inst.write(":SOURce:VOLTage:STEP %.6f" %  step)
+            self.inst.write(":TRIG:COUN %d" % Npoints)         # Set to perform N measurements.
+        elif mode == 'list':
+            
+            self.inst.write(":SOURce:VOLTage:MODE LIST")
+            Npoints = len(sweep_list)
+            t = ''
+            for value in sweep_list:
+                t += f'{value:.4f},'
+            t = t[0:-1]
+            
+            self.inst.write(":SOURce:LIST:VOLTage %s" % t)
+            self.inst.write(":TRIG:COUN %d" % Npoints)  
+        
+        if isinstance(delay, str):
+            if delay.lower() == 'auto':
+                self.inst.write(":SOURce:DELay:AUTO ON")
+                self.sweep_total_time = (0.005  + nplc /50 + 0.05) * Npoints
+            else:
+                raise ValueError('Expected "auto" or a float in the delay kwarg')
+        else:
+            self.inst.write(":SOURce:DELay %.6f" % delay)
+            self.sweep_total_time = (delay  + nplc /50 + 0.05) * Npoints
+        
+        if isinstance(source_range, str):
+            self.inst.write(":SOURce:SWEep:RANGing %s" % source_range.upper())
+        elif isinstance(source_range, (float, int)):
+            self.inst.write(":SOURce:SwEep:RANging FIXED")
+            self.inst.write(":SOURCe:VOLTage:RANGing %.6e" % source_range)
+        else:
+            raise ValueError(f'Source ranging type {type(source_range)} not accepted.')
+        
+        self.inst.write("SENSe:FUNC:CONC OFF")
+        self.inst.write(":SENSe:FUNC 'CURR:DC'")
+        self.inst.write(":SENSe:CURR:NPLC %.3f" % nplc)      # Set measurement speed to n PLC.
+        self.inst.write(":SENSe:CURR:PROT:LEV %.3g" % cmpl)
+        
+        if isinstance(ranging, str):
+            if ranging.lower() == 'AUTO':
+                self.inst.write(":SENSe:CURRent:RANGe:AUTO ON")
+        elif isinstance(ranging, (int, float)):
+            self.inst.write(":SENSe:CURRent:RANGe:AUTO OFF")
+            if ranging >= cmpl:
+                print('INFO: The compliance is increased to match the SENSe range')
+                self.inst.write(":SENSe:CURR:PROT:LEV %.3e" % ranging)
+            self.inst.write(":SENSe:CURRent:RANGe %.6e" % ranging)
+        else:
+            raise ValueError(f'Sense ranging type {type(ranging)} not accepted.')
+            
+            
+    def configure_syncsweep_slave(self, bias_voltage = -5.0, reset = True, Npoints = 1, nplc = 1, source_delay = 0, trig_delay = 0, curr_cmpl = 0.01, ranging = 'AUTO'):
+        
+        if reset:
+            self.inst.write("*RST")                  # Reset instrument to default parameters if reset flag is True.
+#            self.inst.write(":SYSTem:TIME:RESet")   # Reset the time of the sourcemeter
+            
+        self.inst.write("trig:cle") # Clear any pending triggers
+        self.inst.write("syst:azer oN") # Ensure auto zero is enabled
+        self.inst.write("form:elem curr") # Send only current readings to PC
+#        self.inst.write("sour:cle:auto on") # Automatically turn output ON/OFF
+        self.inst.write("sour:cle:auto:mode tco") # Turn off after trigger count
+        # The following section configures the Trigger Model for the PD
+        # SourceMeter instrument.
+        self.inst.write("arm:coun 1") # Perform only one scan
+        self.inst.write("arm:sour imm") # Immediately start scan
+        self.inst.write("arm:dir acc") # Wait for trigger event (IMM)
+        self.inst.write("arm:outp none") # No output triggers from scan
+        self.inst.write("trig:coun {:d}".format(Npoints)) # Perform # points in sweep
+        self.inst.write("trig:sour tlin") # Trigger using Trigger Link
+        self.inst.write("trig:dir acc") # Wait for first trigger
+        self.inst.write("trig:inp sour") # Wait for trig before source "on"
+        self.inst.write("trig:outp del") # Output trigger after delay
+        self.inst.write("trig:ilin 2") # Input trigger line
+        self.inst.write("trig:olin 1") # Output trigger line
+        self.inst.write("trig:del {:.4f}".format(trig_delay)) # Set trigger delay before source
+        
+        self.inst.write("sour:func volt") # Output constant voltage
+        self.inst.write("sens:func 'curr'") # Measure leakage current of PD
+        self.inst.write("sour:curr:rang:auto on") # Enable source auto range
+        self.inst.write("sour:volt {:.4f}".format(bias_voltage)) # Set output level
+        self.inst.write("sour:del {:.4f}".format(source_delay)) # Set source delay before measure
+        self.inst.write("sens:curr:prot {:.4e}".format(curr_cmpl)) # 10mA compliance range
+#        self.inst.write("sens:curr:rang:AUTO ON") # Measure on the specified range
+        
+        if isinstance(ranging, str):
+            if ranging.lower() == 'AUTO':
+                self.inst.write(":SENSe:CURRent:RANGe:AUTO ON")
+        elif isinstance(ranging, (int, float)):
+            self.inst.write(":SENSe:CURRent:RANGe:AUTO OFF")
+            if ranging >= curr_cmpl:
+                print('INFO: The compliance is increased to match the SENSe range')
+                self.inst.write(":SENSe:CURR:PROT:LEV %.3e" % ranging)
+            self.inst.write(":SENSe:CURRent:RANGe %.6e" % ranging)
+            
+        else:
+            raise ValueError(f'Sense ranging type {type(ranging)} not accepted.')
+            
+        self.inst.write("sens:curr:nplc {:.4f}".format(nplc)) # Measurement integration
+        
+#        self.inst.write('INIT')
+        
+        
+    def fetch(self, delay = None):
+        
+        if delay == None:
+            delay = self.sweep_total_time
+        # Fetch the data
+        return self.inst.query_ascii_values('FETCH?',delay = delay, container = array)  # Assumes data is in ASCII format
         
